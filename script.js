dee508055bef6eeb02b413484f084b5c966a3faa
Zion-Ma <65025538+Zion-Ma@users.noreply.github.com>
2024-11-11 13:16:02 -0500

    Add components

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("header nav ul li a");
+    
+    navLinks.forEach(link => {
+        link.addEventListener("click", smoothScroll);
+    });
+
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute("href").substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+});
